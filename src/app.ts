c08efa38b026339fbecfb6942fcf7e9844c8e0b9
Valentino Spinaci <spinacivalentino@gmail.com>
2026-05-16 15:05:38 -0300

    Conexion de Prisma y mySQL

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
-import express from 'express';
+import express from 'express'
+import prisma from './config/prisma'
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.get('/health', (req, res) => {
-    res.json({ status: 'ok', message: 'API de Changuita corriendo' })
+app.get('/health', async (req, res) => {
+    try {
+        await prisma.$queryRaw`SELECT 1`
+        res.json({ status: 'ok', message: 'API de Changuita corriendo', db: 'conectada' })
+    } catch (error) {
+        res.status(500).json({ status: 'error', db: 'desconectada' })
+    }
 })
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`)
 })
 
-export default app;
+export default app
